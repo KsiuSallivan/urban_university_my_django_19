e5ffcf90124ac6936ff4a7d2dbdb1b90548855de
KsiuSallivan <ksiu.sallivan@gmail.com>
2025-01-12 18:39:10 +0300

    module 19 task1 QuerySet

--- django_module_19/task1/models.py
+++ django_module_19/task1/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Buyer(models.Model):
-    name = models.CharField(max_length=100)  # Имя покупателя
+    name = models.CharField(max_length=110)  # Имя покупателя
     balance = models.DecimalField(max_digits=10, decimal_places=2)  # Баланс
     age = models.PositiveIntegerField()  # Возраст
 
